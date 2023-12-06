ff0703681537664b79914d3be14dcd54e7cc0b08
Drew <silverbird337@gmail.com>
2023-12-06 17:46:54 -0500

    attempt script execution upon file save

--- monitor_passwords.sh
+++ monitor_passwords.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-code
+# Adding to startup:
+# crontab -e
+# @reboot sleep 5;/home/drew/Documents/github/secure-password-storage/monitor_passwords.sh
+
+# dependencies:
+# inotify-tools
+PASSWORDS_PATH=/home/drew/Desktop/Passwords.kdbx
+
+inotifywait -m -e close_write "${PASSWORDS_PATH}" |
+    while read -r filename event; do
+        echo "working"
+    done
